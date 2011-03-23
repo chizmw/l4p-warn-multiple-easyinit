07b4b3835dd57e2426b6a45605413d885b90ab44
Chisel <chisel@chizography.net>
2011-03-23 21:06:09 +0000

    Initial commit

---
+++ lib/Log/Log4perl/Warn/Multiple/EasyInit.pm
@@ -0,0 +1,42 @@
+package Log::Log4perl::Warn::Multiple::EasyInit;
+# ABSTRACT: trap multiple calls to Log::Log4perl::easy_init
+use strict;
+use warnings;
+
+use Carp;
+use Log::Log4perl;
+
+our ($package, $filename, $line);
+
+sub set_trap {
+    my $code = \&Log::Log4perl::easy_init;
+    no warnings 'redefine';
+    *Log::Log4perl::easy_init = sub {
+        if(Log::Log4perl->initialized) {
+            carp( "Log::Log4perl already initialised with easy_init() [at $filename, line $line]" );
+        }
+        else {
+            # store our first initialisation
+            ($package, $filename, $line) = caller;
+        }
+        # run the original function
+        &$code;
+    };
+}
+
+BEGIN {
+    set_trap;
+}
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+  BEGIN {
+    use Log::Log4perl::Warn::Multiple::EasyInit;
+  }
+
+=cut
